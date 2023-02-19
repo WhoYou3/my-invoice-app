87d75b139e73b0304dc85dbf2c691ab1cb6a48f1
Jan Irzyk <janirzyk333@gmail.com>
2023-02-19 15:31:40 +0100

    define root for colors

---
+++ src/Containers/Navbar/Navbar.tsx
@@ -0,0 +1,17 @@
+import "./navbar.css";
+import login from "../../assets1/logo.svg";
+
+const Navbar: React.FC = () => {
+  return (
+    <nav>
+      <div>
+        <img src="login" alt="" />
+      </div>
+      <div></div>
+      <div />
+      <div></div>
+    </nav>
+  );
+};
+
+export default Navbar;

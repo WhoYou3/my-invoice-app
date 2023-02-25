4549b95cbc426018090ed53174df5d21559a8b2f
Jan Irzyk <janirzyk333@gmail.com>
2023-02-25 16:45:55 +0100

    Design detail
    Preapre design for detail carad

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,20 @@ import { Navbar, Invoices, InvoiceMaker } from "./Containers";
 import { useAppSelector } from "./store/store";
 import { motion } from "framer-motion";
 import "./App.css";
+import { InvoiceDetail } from "./components";
 
 const App: React.FC = () => {
   const isDarkMode = useAppSelector((state) => state.toggle.isDark);
   const isOpenForm = useAppSelector((state) => state.isAdding.isAdding);
+  const isDetailCard = useAppSelector(
+    (state) => state.isOpenDetail.isShowDetail
+  );
 
   return (
     <div className={`app-container ${isDarkMode ? "dark" : "light"} `}>
       <Navbar />
       <Invoices />
+
       {isOpenForm ? <InvoiceMaker /> : null}
     </div>
   );

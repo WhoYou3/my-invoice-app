7ec298eccc1419dbd53d78a2fdf188b5276de925
Jan Irzyk <janirzyk333@gmail.com>
2023-02-25 13:27:05 +0100

    Clean code
    Clean some useless imports

--- src/Containers/Invoices/Invoices.tsx
+++ src/Containers/Invoices/Invoices.tsx
@@ -1,38 +1,12 @@
 import { useEffect, useState } from "react";
 import { db } from "../../firebase/firebase-config";
 import { InvoiceType } from "./InvoiceType";
-import {
-  collection,
-  onSnapshot,
-  getDocs,
-  CollectionReference,
-  QuerySnapshot,
-} from "firebase/firestore";
-
-import { DocumentData } from "firebase/firestore";
+import { collection, onSnapshot, QuerySnapshot } from "firebase/firestore";
 import { Invoice, InvoiceNavigation } from "../../components/index";
 import "./Invoices.css";
 
 const Invoices = () => {
   const [invoices, setInvoices] = useState<InvoiceType[]>([]);
-
-  const invoicesCollectionRef: CollectionReference<DocumentData> = collection(
-    db,
-    "Invoices"
-  );
-
-  // useEffect(() => {
-  //   const getUsers = async () => {
-  //     const data = await getDocs(invoicesCollectionRef);
-  //     const invoicesArray = data.docs.map((singleInvoice) => ({
-  //       ...singleInvoice.data(),
-  //       id: singleInvoice.id,
-  //     })) as InvoiceType[];
-  //     setInvoices(invoicesArray);
-  //   };
-
-  //   getUsers();
-  // }, []);
 
   useEffect(() => {
     const invoicesCollectionRef = collection(db, "Invoices");
